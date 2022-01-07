1be85b534b16a2c5b2e8425fc6962474a54df858
jeremy <244161669@qq.com>
2022-01-07 18:15:15 +0800

    初始化配置

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div class="test"></div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="scss">
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
+
+.test {
+  width: 100px;
+  height: 100px;
+  background-color: #f00;
+  transform: translate(100px,100px);
+}
+</style>
